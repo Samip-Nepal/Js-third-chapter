93322a9e71b3fbd96625e35dca2beb40278ed6ac
Samip-Nepal <samipnepal5@email.com>
2024-02-05 23:24:39 +0545

    For loop

---
+++ first.js
@@ -0,0 +1,12 @@
+//for loop
+for(let i=1;i<=100;i++){
+    console.log("Samip");
+}
+let sum=0;
+for(let i=1;i<=100;i++){
+    sum=sum+i;
+}
+console.log("Sum=", sum);
+for(let i=1;i<=100;i++){
+    console.log("i=",i);
+}
