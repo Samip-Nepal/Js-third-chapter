57d259fd66166800ba90b8ed53da5f546fb414a5
Samip-Nepal <samipnepal5@email.com>
2024-02-06 23:41:31 +0545

    For of loop use for string and array for displaying char

--- first.js
+++ first.js
@@ -1,9 +1,9 @@
-//do while loop 
-let i=1;
-do{
-    console.log("i=",i);
-    i++;
-}while (i<=5);
-
+let size=0;
+let str="Samipnepal"
+for(let val of str){
+console.log("val=",val);
+size++;
+}
+console.log(size);
 
 
