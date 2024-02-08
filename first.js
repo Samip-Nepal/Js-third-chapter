44b4e4a0eb12c4a6a6c71fe5d8eb25f7840d135d
Samip-Nepal <samipnepal5@email.com>
2024-02-08 07:25:04 +0545

    Adding for in loop use for object

--- first.js
+++ first.js
@@ -1,3 +1,17 @@
+//for in loop
+
+const student ={
+    Name:"Samip",
+    Surname:"Nepal",
+    age:21,
+    type:"Normal",
+    isreading:true,
+};
+for (let  key in student) {
+    console.log("Keys=", key);
+}
+
+//for of loop
 let size=0;
 let str="Samipnepal"
 for(let val of str){
