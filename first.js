5b5faee536df11e87d21c1cad24760b684d0ec38
Samip-Nepal <samipnepal5@email.com>
2024-02-09 17:14:04 +0545

    String and some property

--- first.js
+++ first.js
@@ -1,45 +1,53 @@
-//Pratice qn2
-let a=10;
- let b=prompt("Tell any number");
-while(b!=a){
-b=prompt("Your guess number is wrong try again");
-}
-console.log("Your guess is correct");
-
-
-
-//Pratice qn 1
-for (let i= 0; i <=100; i++) {
-    if(i%2===0){
-        console.log(i);
-    }   
-}
+//string
+let str1="Samip";
+let str2="Nepal";
+console.log(str1,str2);
+console.log(str1.length);
+console.log(str2[0]);
 
 
 
 
 
+
+
+//Pratice q
+// let a=10;
+//  let b=prompt("Tell any number");
+// while(b!=a){
+// b=prompt("Your guess number is wrong try again");
+// }
+// console.log("Your guess is correct");
+
+
+
+// //Pratice qn 1
+// for (let i= 0; i <=100; i++) {
+//     if(i%2===0){
+//         console.log(i);
+//     }   
+// }
 
 //for in loop
 
-const student ={
-    Name:"Samip",
-    Surname:"Nepal",
-    age:21,
-    type:"Normal",
-    isreading:true,
-};
-for (let  key in student) {
-    console.log("Keys value=", student [key]);
-}
+// const student ={
+//     Name:"Samip",
+//     Surname:"Nepal",
+//     age:21,
+//     type:"Normal",
+//     isreading:true,
+// };
+// for (let  key in student) {
+//     console.log("Keys value=", student [key]);
+// }
 
-//for of loop
-let size=0;
-let str="Samipnepal"
-for(let val of str){
-console.log("val=",val);
-size++;
-}
-console.log(size);
+// //for of loop
+// let size=0;
+// let str="Samipnepal"
+// for(let val of str){
+// console.log("val=",val);
+// size++;
+// }
+// console.log(size);
 
 
